a53b1b0e7a24a5834b3f01c9b06c0b35f6bf261b
aicma <Alec7@web.de>
2016-05-03 10:42:28 +0200

    heroku

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 var http = require('http');
 var url = require('url');
 
@@ -24,5 +25,26 @@ var server = http.createServer(function(req, res){
 server.listen(PORT, function(){
   console.log('listening on ' + PORT);
 });
+*/
 
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send({
+    1: {
+      lat: 48.3582,
+      long: 10.9067
+    },
+    2: {
+      lat: 23.6578,
+      long: 47.3267
+      }
+  });
+});
+
+app.listen(8080, function () {
+  console.log('App listening on port 8080!');
+});
 
