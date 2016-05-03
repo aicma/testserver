e94645ef3dbd8b7776b88a34986b32a3f2578e37
aicma <Alec7@web.de>
2016-05-03 19:41:59 +0200

    debug2

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', function(req, res) {
-  console.log('test');
+  res.send('test');
 /*
   res.send('post request received');
   req.on('end',function(data){
