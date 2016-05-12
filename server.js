267ccace87d1228e3dbc613c351960d3ec125775
aicma <Alec7@web.de>
2016-05-12 11:56:36 +0200

    started to REST2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 var PORT = process.env.PORT || 8080;
 
-app.get('/', function (req, res) {
+app.get('/cameras', function (req, res) {
   res.send({
     1: {
       lat: 48.3582,
@@ -17,7 +17,34 @@ app.get('/', function (req, res) {
   );
 });
 
-app.post('/', function(req, res) {
+app.get('/events', function (req, res) {
+  res.send({
+     {
+      id: 0,
+      name: "Zugspitz Ultratrail",
+      date: new Date("2016-06-23"),
+      img: "",
+      numberformat: /^\d{4}$/
+    },
+    {
+      id: 1,
+      name: "Rad am Ring",
+      date: new Date("2016-07-23"),
+      img: "",
+      numberformat: /^[A-B]{1}\s*\d{1,4}$/i
+    },
+    {
+      id: 2,
+      name: "TestEvent",
+      date: new Date("2016-02-23"),
+      img: "",
+      numberformat: /^\d{1,4}\s*[A-B]$/i
+    }
+  }
+  );
+});
+
+app.post('/rider', function(req, res) {
   var jsonString = '';
 
   req.on('data', function (data) {
