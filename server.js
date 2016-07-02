212f33924628953a80bbca5fb3811029f4303971
aicma <Alec7@web.de>
2016-07-02 16:05:54 +0200

    neues testevent

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var PORT = process.env.PORT || 8080;
 app.get('/cameras', function (req, res) {
   var eventID = req.query.id;
   console.log(eventID +', '+ req.query.id);
-  res.header("Access-Control-Allow-Origin", "*");
+  
   switch (parseInt(eventID, 10)) {
     case 0:
-      res.send({
+      res.status(200).send({
           1: {
             lat: 48.3582,
             long: 10.9067
@@ -21,7 +21,7 @@ app.get('/cameras', function (req, res) {
       });
       break;
     case 1:
-      res.send({
+      res.status(200).send({
           1: {
             lat: 48.3582,
             long: 10.9067
@@ -33,7 +33,7 @@ app.get('/cameras', function (req, res) {
         });
       break;
     case 2:
-      res.send({
+      res.status(200).send({
           1: {
             lat: 48.3582,
             long: 10.9067
@@ -45,7 +45,7 @@ app.get('/cameras', function (req, res) {
       });
       break;
     case 3245:
-      res.send({
+      res.status(200).send({
           1: {
             lat: 48.3582,
             long: 10.9067
@@ -56,8 +56,31 @@ app.get('/cameras', function (req, res) {
           }
       });
       break;
+    case 7357:
+      res.status(200).send({
+          1: {
+            lat: 48.345744, 
+            long: 10.924400
+          },
+          2: {
+            lat: 48.344322, 
+            long: 10.936389
+          },
+          3:{
+            lat: 48.413699, 
+            long: 10.889720
+          },
+          4:{
+            lat: 48.375427, 
+            long: 10.877972
+          },
+          5:{
+            lat: 48.359803, 
+            long: 10.883200
+          }
+      });
     default:
-        res.send('ERROR - EventID not registered'); //technically should not happen         
+        res.status(404).send('ERROR - EventID not registered'); //technically should not happen         
   }
 });
 
@@ -91,6 +114,13 @@ app.get('/events', function (req, res) {
       date: new Date("2016-06-23"),
       img: "",
       numberformat: "^[A-B]{1}\\s*\\d{1,4}$"
+    },
+    {
+      id: 7357,
+      name: "Augsburg Halbmarathon",
+      date: new Date("2016-07-02"),
+      img: "",
+      numberformat: "^[A-Z]{1}\\s*\\d{1,4}$"
     }
   ]);
 });
